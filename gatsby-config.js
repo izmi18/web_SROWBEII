a95b102600ba967872ad0c414296c0a6a6943140
izmii06 <izminurmaulidina@gmail.com>
2025-06-23 16:54:26 +0700

    tambah seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: "SROWBEII",
     description:"Srowbeii merupakan wadah bagi kerajinan tangan yang autentik, dan bisa custom sesuai keinginan anda. unik, personal dan dibuat dengan cinta oleh pengrajinnya.",
     author: "izmi",
-    keywords: "jakarta, cengkareng, souvenir, handmade, handycraft, terdekat, aksesoris, gelang, kalung, amigurami,"
+    keywords: "jakarta, cengkareng, souvenir, handmade, handycraft, terdekat, aksesoris, gelang, kalung, amigurami, beads, bagcharm, keychain, accessories, custom, gelang manik, diy, bracelet, phonestarp, jual"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
